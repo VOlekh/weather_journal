70366157397a93e3e2616e611fe5534a866814a1
Valentina-Olekhnovich <volekh@gmail.com>
2020-06-30 14:39:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,40 @@
 projectData = {};
 
 // Require Express to run server and routes
+//The project file server.js should require express()
+const express = reqyuire ('express');
 
 // Start up an instance of app
+//The project file server.js should create an instance of their app using express.
+const app = express();
+
+//NOT DEFINED The Express app instance should be pointed to the project folder with .html, .css, and .js files.
 
 /* Middleware*/
+//The body-parser package should be installed and included in the project.
+const bodyParser = require('body-parser')
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+//The instance of the app should be setup to use cors()
+const cors = require('cors');
 
+//NOT AS IT IS NOW The Express app instance should be pointed to the project folder with .html, .css, and .js files.
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
-// Setup Server
+
+
+
+// Spin up the server
+
+// Callback to debug
+
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+// Post Route
